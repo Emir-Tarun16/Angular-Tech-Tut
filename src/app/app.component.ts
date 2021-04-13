9a67b9382cd6ef05050d67046d1b354fd40c9e67
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-04-14 01:42:38 +0530

    counter app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+
+  count: number = 0;
+
+  onHandleIncrease = () => {
+    if(this.count === 11){
+      return;
+    }
+    this.count = this.count + 1;
+    console.log("increse is working")
+  }
+
+  onHandleDecrease = () => {
+    if(this.count === 0){
+      return;
+    }
+    this.count = this.count - 1;
+    console.log("decrease is working")
+  }
+
+  onHandleReset = () =>{
+    this.count = 0;
+    console.log("Reset is working")
+  }
 }

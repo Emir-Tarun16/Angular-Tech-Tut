5794c6653031693c15af3a0e4031e64f71353c47
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-04-22 01:14:44 +0530

    word generator app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import arrayWords from "../utils/words"
+import CountryWords from "../utils/countries";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wordgen-app';
+
+  word = '';
+  countryword = '';
+  limit = 10;
+
+  handleSlideChange = (newLimit: number) => {
+      this.limit = newLimit;
+  }
+
+  generator = () =>{
+    this.word = arrayWords.slice(0, this.limit).join(' ');
+  }
+
+  countryGenerator = () =>{
+    console.info("countries function working")
+    this.countryword = CountryWords.slice(0,this.limit).join(' ');
+  }
 }

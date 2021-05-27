1d9e92c5d05616a114298492d41bbbaaad5fd08b
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-05-27 22:40:38 +0530

    Web api project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,    // Req Browser Animation Module 
+    ToastrModule.forRoot(), //Toaster Module Added
+    FontAwesomeModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faDatabase, faEnvelope, faMapMarkedAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input() user;
+
+  faEnvelope = faEnvelope;
+  faMapMarkedAlt = faMapMarkedAlt;
+  faPhone = faPhone;
+  faDatabase = faDatabase;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

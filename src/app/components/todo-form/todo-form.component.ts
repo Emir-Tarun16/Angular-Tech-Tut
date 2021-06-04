101116cde8013f6e4be608eae1e2b304fcd62b79
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-06-04 20:18:10 +0530

    todo app completed

---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core'
+import { Todo } from './../../model/Todo';
+import {v4 as uuidV4 } from 'uuid';
+import { TodoService } from 'src/app/service/todo.service';
+
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.css']
+})
+export class TodoFormComponent implements OnInit {
+
+  todoTitle: string;
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+
+  }
+
+
+  handleAdd = () =>{
+    const newTodo: Todo = {
+      id: uuidV4,
+      title: this.todoTitle,
+      isComplete: false,
+      date: new Date(),
+  
+    }
+
+  this.todoService.addTodos(newTodo);
+  this.todoTitle ="";
+  }
+
+
+}
